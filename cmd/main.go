38d890bd09c326f7791f42028cdf62161cbc49d2
Ellin Park <1833921+Ellin@users.noreply.github.com>
2026-09-09 16:07:59 +0300

    feat: set up database connection pool

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"forum/internal/config"
+	"forum/internal/database"
 	"log/slog"
 )
 
 func main() {
 	cfg := config.Load()
 	slog.Info("config loaded:", "config", cfg)
+
+	db, err := database.Open(cfg.DbPath)
+	if err != nil {
+		slog.Error("opening database", "err", err)
+		return
+	}
+	defer db.Close()
+	slog.Info("connected to database", "db", cfg.DbPath)
 }
--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,24 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// Open initializes a database connection pool for a given database path (connection string).
+// The connection is tested before returning the sql.DB connection pool.
+func Open(path string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", path+"?_foreign_keys=on") // enforce foreign keys -> validate existence of rows referred to by foreign keys
+	if err != nil {
+		return nil, fmt.Errorf("open database: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("verify database connection: %w", err)
+	}
+
+	return db, nil
+}
