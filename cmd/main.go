f7831c5a904b19014e5354ba328f7952a4a2319f
Ellin Park <1833921+Ellin@users.noreply.github.com>
2026-09-09 15:45:19 +0300

    feat: load config data from CLI flags

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"forum/internal/config"
+	"log/slog"
+)
 
+func main() {
+	cfg := config.Load()
+	slog.Info("config loaded:", "config", cfg)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,18 @@
 package config
+
+import "flag"
+
+type Config struct {
+	Port   int
+	DbPath string
+}
+
+func Load() Config {
+	cfg := Config{}
+
+	flag.IntVar(&cfg.Port, "port", 8080, "Server port number")
+	flag.StringVar(&cfg.DbPath, "db", "forum.db", "Database file path")
+	flag.Parse()
+
+	return cfg
+}
