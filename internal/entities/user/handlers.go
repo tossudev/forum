2301161cd3f0eaf82b5a7df24e9ae939dd7353f7
Ellin <1833921+Ellin@users.noreply.github.com>
2026-09-12 19:19:01 +0000

    feat: create handler and service layer for user registration

--- internal/entities/user/handlers.go
+++ internal/entities/user/handlers.go
@@ -1,9 +1,47 @@
 package user
 
+import (
+	"encoding/json"
+	"fmt"
+	"forum/internal/errs"
+	"forum/internal/password"
+	"net/http"
+)
+
 type UserHandler struct {
 	service *UserService
 }
 
 func NewHandler(service *UserService) *UserHandler {
 	return &UserHandler{service: service}
+}
+
+func (h *UserHandler) RegisterUser(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var input struct {
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		errs.WriteError(w, fmt.Errorf("%w: invalid request body", errs.ErrInvalidUserInput))
+		return
+	}
+
+	user := User{
+		Username: input.Username,
+		Email:    input.Email,
+		Password: password.Password{},
+	}
+
+	if err := h.service.RegisterUser(ctx, &user, input.Password); err != nil {
+		errs.WriteError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": fmt.Sprint("Welcome to Literary Lions, %s!", user.Username)})
 }
--- internal/entities/user/service.go
+++ internal/entities/user/service.go
@@ -1,9 +1,28 @@
 package user
 
+import (
+	"context"
+	"forum/internal/password"
+)
+
 type UserService struct {
 	repo *UserRepo
 }
 
 func NewService(repo *UserRepo) *UserService {
 	return &UserService{repo: repo}
+}
+
+func (s *UserService) RegisterUser(ctx context.Context, user *User, inputPassword string) error {
+	// TODO: Validate user fields
+
+	// Set password
+	pw, err := password.New(inputPassword)
+	if err != nil {
+		return err
+	}
+
+	user.Password = pw
+
+	return nil
 }
