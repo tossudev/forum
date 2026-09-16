dca9eba2ec3f47a13d1a964f9438e431d36e2e50
Ellin <1833921+Ellin@users.noreply.github.com>
2026-09-16 19:16:52 +0000

    fix: expires_at not set in query

--- internal/session/repository.go
+++ internal/session/repository.go
@@ -15,7 +15,7 @@ func NewRepo(db *sql.DB) *SessionRepo {
 	return &SessionRepo{db: db}
 }
 
-func (r *SessionRepo) AddSession(ctx context.Context, session Session) error {
+func (r *SessionRepo) AddSession(ctx context.Context, session *Session) error {
 	query := `INSERT INTO sessions (id, user_id, csrf_token, date_created, expires_at)
 	VALUES (?, ?, ?, ?);`
 
@@ -34,7 +34,7 @@ func (r *SessionRepo) GetSessionByID(ctx context.Context, id string) (*Session, 
 	s := &Session{}
 	if err := r.db.QueryRowContext(ctx, query, id).Scan(&s.id, &s.userID, &s.csrfToken, &s.createdAt, &s.idleExpiresAt); err != nil {
 		if err == sql.ErrNoRows {
-			return nil, fmt.Errorf("%w: invalid session ID", errs.ErrsUnauthorized)
+			return nil, fmt.Errorf("%w: invalid session ID", errs.ErrNotFound)
 		}
 		return nil, fmt.Errorf("GetSessionByID: scanning row: %w", err)
 	}
@@ -51,9 +51,9 @@ func (r *SessionRepo) DeleteSession(ctx context.Context, id string) error {
 	return nil
 }
 
-func (r *SessionRepo) UpdateExpiry(ctx context.Context, id string) error {
-	query := `UPDATE sessions SET expires_at WHERE id = ?;`
-	if _, err := r.db.ExecContext(ctx, query, id); err != nil {
+func (r *SessionRepo) UpdateExpiry(ctx context.Context, session *Session) error {
+	query := `UPDATE sessions SET expires_at = ? WHERE id = ?;`
+	if _, err := r.db.ExecContext(ctx, query, session.idleExpiresAt, session.id); err != nil {
 		return fmt.Errorf("refreshing session expiry: %w", err)
 	}
 
