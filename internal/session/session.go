38cc8fab3648735f316199f1a130863dd413e3fa
Ellin Park <1833921+Ellin@users.noreply.github.com>
2026-09-16 12:23:44 +0300

    feat: add cookie writer

--- internal/session/session.go
+++ internal/session/session.go
@@ -4,31 +4,29 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
+	"net/http"
 	"time"
 )
 
 type SessionManager struct {
-	repo               *SessionRepo
-	cookieName         string
-	idleExpiration     time.Duration
-	absoluteExpiration time.Duration
+	repo           *SessionRepo
+	cookieName     string
+	idleExpiration time.Duration
 }
 
 type Session struct {
-	id                string
-	csrfToken         string
-	userID            int
-	createdAt         time.Time
-	idleExpiresAt     time.Time
-	absoluteExpiresAt time.Time
+	id            string
+	csrfToken     string
+	userID        int
+	createdAt     time.Time
+	idleExpiresAt time.Time
 }
 
 func NewSessionManager(repo *SessionRepo, cookieName string, idleExpiration, absoluteExpiration time.Duration) *SessionManager {
 	return &SessionManager{
-		repo:               repo,
-		cookieName:         cookieName,
-		idleExpiration:     idleExpiration,
-		absoluteExpiration: absoluteExpiration,
+		repo:           repo,
+		cookieName:     cookieName,
+		idleExpiration: idleExpiration,
 	}
 }
 
@@ -46,12 +44,11 @@ func (sm *SessionManager) NewSession(userID int) (*Session, error) {
 	now := time.Now()
 
 	s := Session{
-		id:                sessionID,
-		csrfToken:         csrfToken,
-		userID:            userID,
-		createdAt:         now,
-		idleExpiresAt:     now.Add(sm.idleExpiration),
-		absoluteExpiresAt: now.Add(sm.absoluteExpiration),
+		id:            sessionID,
+		csrfToken:     csrfToken,
+		userID:        userID,
+		createdAt:     now,
+		idleExpiresAt: now.Add(sm.idleExpiration),
 	}
 
 	return &s, nil
@@ -66,4 +63,18 @@ func generateToken(length int) (string, error) {
 	}
 
 	return base64.RawURLEncoding.EncodeToString(token), nil
+}
+
+func (sm *SessionManager) writeCookie(w http.ResponseWriter, session *Session) {
+	cookie := &http.Cookie{
+		Name:     sm.cookieName,
+		Value:    session.id,
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+		// Secure: true, // TODO: Use when HTTPS is implemented
+		Expires: session.idleExpiresAt,
+		MaxAge:  int(sm.idleExpiration / time.Second),
+	}
+
+	http.SetCookie(w, cookie)
 }
