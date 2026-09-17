fa7716d5d7b92ae0b685b8dfadd584ac92d87c3c
Ellin Park <1833921+Ellin@users.noreply.github.com>
2026-09-17 14:42:00 +0300

    fix: wrong type when inserting and getting time stamps

--- internal/session/repository.go
+++ internal/session/repository.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"forum/internal/config"
 	"forum/internal/errs"
+	"time"
 )
 
 type SessionRepo struct {
@@ -17,9 +19,12 @@ func NewRepo(db *sql.DB) *SessionRepo {
 
 func (r *SessionRepo) addSession(ctx context.Context, session *Session) error {
 	query := `INSERT INTO sessions (id, user_id, csrf_token, date_created, expires_at)
-	VALUES (?, ?, ?, ?);`
+	VALUES (?, ?, ?, ?, ?);`
 
-	_, err := r.db.ExecContext(ctx, query, session.id, session.userID, session.csrfToken, session.createdAt, session.idleExpiresAt)
+	createdAtStr := session.createdAt.Format(config.TimeFormat)
+	expiresAtStr := session.idleExpiresAt.Format(config.TimeFormat)
+
+	_, err := r.db.ExecContext(ctx, query, session.id, session.userID, session.csrfToken, createdAtStr, expiresAtStr)
 	if err != nil {
 		return fmt.Errorf("adding session: %w", err)
 	}
@@ -28,18 +33,32 @@ func (r *SessionRepo) addSession(ctx context.Context, session *Session) error {
 }
 
 func (r *SessionRepo) getSessionByID(ctx context.Context, id string) (*Session, error) {
-	query := `SELECT (id, user_id, csrf_token, date_created, expires_at)
+	query := `SELECT id, user_id, csrf_token, date_created, expires_at
 	FROM sessions WHERE id = ?;`
 
-	s := &Session{}
-	if err := r.db.QueryRowContext(ctx, query, id).Scan(&s.id, &s.userID, &s.csrfToken, &s.createdAt, &s.idleExpiresAt); err != nil {
+	var s Session
+	var createdAtStr string
+	var expiresAtStr string
+	var parseErr error
+
+	if err := r.db.QueryRowContext(ctx, query, id).Scan(&s.id, &s.userID, &s.csrfToken, &createdAtStr, &expiresAtStr); err != nil {
 		if err == sql.ErrNoRows {
 			return nil, fmt.Errorf("%w: invalid session ID", errs.ErrNotFound)
 		}
-		return nil, fmt.Errorf("GetSessionByID: scanning row: %w", err)
+		return nil, fmt.Errorf("getSessionByID: scanning row: %w", err)
 	}
 
-	return s, nil
+	s.createdAt, parseErr = time.Parse(config.TimeFormat, createdAtStr)
+	if parseErr != nil {
+		return nil, fmt.Errorf("getSessionByID: created at time parsing failure: %w", parseErr)
+	}
+
+	s.idleExpiresAt, parseErr = time.Parse(config.TimeFormat, expiresAtStr)
+	if parseErr != nil {
+		return nil, fmt.Errorf("getSessionByID: expires at time parsing failure: %w", parseErr)
+	}
+
+	return &s, nil
 }
 
 func (r *SessionRepo) deleteSession(ctx context.Context, id string) error {
@@ -53,7 +72,9 @@ func (r *SessionRepo) deleteSession(ctx context.Context, id string) error {
 
 func (r *SessionRepo) updateExpiry(ctx context.Context, session *Session) error {
 	query := `UPDATE sessions SET expires_at = ? WHERE id = ?;`
-	if _, err := r.db.ExecContext(ctx, query, session.idleExpiresAt, session.id); err != nil {
+
+	expiresAtStr := session.idleExpiresAt.Format(config.TimeFormat)
+	if _, err := r.db.ExecContext(ctx, query, expiresAtStr, session.id); err != nil {
 		return fmt.Errorf("refreshing session expiry: %w", err)
 	}
 
